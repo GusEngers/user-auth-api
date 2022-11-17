ab4e8ca4d2507321f1a22b8f6e971a5f2fad7be9
GusEngers <gustavoengers6441@gmail.com>
2022-11-17 11:06:49 -0300

    Correccion de detalles

--- controllers/users/usersList.js
+++ controllers/users/usersList.js
@@ -1,15 +1,29 @@
 'use strict';
 const User = require('../../models/user.js');
 
+const dataUser = (user) => {
+	return {
+		_id: user._id,
+		name: user.name,
+		email: user.email,
+		status: user.status,
+		date: user.date,
+		data: user.data
+	};
+};
+
 const usersList = async (api_key, status) => {
 	switch (status) {
 		case undefined:
 		case '':
-			return await User.find({api_key});
+			let all = await User.find({api_key});
+			return all.map(user => dataUser(user));
 		case 'active':
-			return await User.find({api_key, status});
+			let active = await User.find({api_key, status})
+			return active.map(user => dataUser(user));
 		case 'inactive':
-			return await User.find({api_key, status});
+			let inactive = await User.find({api_key, status});
+			return inactive.map(user => dataUser(user));
 		default:
 			throw new Error('An unexpected error has occurred');
 	};
