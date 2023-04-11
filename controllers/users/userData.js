f3a04925c7f144bd2a9b9e9c24b67d89eac182a3
GusEngers <gustavoengers6441@gmail.com>
2023-04-11 18:01:57 -0300

    lista de usuarios

--- controllers/users/userData.js
+++ controllers/users/userData.js
@@ -1,13 +1,11 @@
 'use strict';
 const User = require('../../models/user.js');
 const { dataUser } = require('./usersList.js');
-const { verificationObjectId } = require('../api_key/verificationApiKey.js');
 
-const userData = async (api_key, id) => {
-	if(!verificationObjectId(id)) throw new Error('Invalid ID');
-	const data = await User.findOne({api_key, _id: id});
-	if(!data) throw new Error('User not found!');
-	return dataUser(data);
+const userData = async (id) => {
+  const data = await User.findById(id);
+  if (!data) throw new Error('User not found!');
+  return dataUser(data);
 };
 
 module.exports = userData;
--- controllers/users/usersList.js
+++ controllers/users/usersList.js
@@ -2,34 +2,34 @@
 const User = require('../../models/user.js');
 
 const dataUser = (user) => {
-	return {
-		id: user._id,
-		name: user.name,
-		email: user.email,
-		status: user.status,
-		date: user.date,
-		data: user.data
-	};
+  return {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    status: user.status,
+    date: user.date,
+    data: user.data,
+  };
 };
 
-const usersList = async (api_key, status) => {
-	switch (status) {
-		case undefined:
-		case '':
-			let all = await User.find({api_key});
-			return all.map(user => dataUser(user));
-		case 'active':
-			let active = await User.find({api_key, status})
-			return active.map(user => dataUser(user));
-		case 'inactive':
-			let inactive = await User.find({api_key, status});
-			return inactive.map(user => dataUser(user));
-		default:
-			throw new Error('An unexpected error has occurred');
-	};
-};
+async function usersList(status) {
+  switch (status) {
+    case undefined:
+    case '':
+      let all = await User.find();
+      return all.map((user) => dataUser(user));
+    case 'active':
+      let active = await User.find({ status });
+      return active.map((user) => dataUser(user));
+    case 'inactive':
+      let inactive = await User.find({ status });
+      return inactive.map((user) => dataUser(user));
+    default:
+      throw new Error('An unexpected error has occurred');
+  }
+}
 
 module.exports = {
-	usersList,
-	dataUser
+  usersList,
+  dataUser,
 };
