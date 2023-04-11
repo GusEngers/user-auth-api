976f78d25bb9e19f60529432fa37b1abc32ba81a
GusEngers <gustavoengers6441@gmail.com>
2023-04-11 18:07:47 -0300

    eliminar y restaurar usuario

--- controllers/users/delete.js
+++ controllers/users/delete.js
@@ -1,22 +1,19 @@
 'use strict';
 const User = require('../../models/user.js');
-const { verificationObjectId } = require('../api_key/verificationApiKey.js');
 
-const logicalDelete = async (api_key, id) => {
-	if(!verificationObjectId(id)) throw new Error('Invalid ID');
-	let data = await User.findOneAndUpdate({_id: id, api_key}, {status: 'inactive'});
-	if(!data) throw new Error('User not found');
-	return 'User deleted';
+const logicalDelete = async (id) => {
+  let data = await User.findByIdAndUpdate(id, { status: 'inactive' });
+  if (!data) throw new Error('User not found');
+  return 'User deleted';
 };
 
-const definitiveDelete = async (api_key, id) => {
-	if(!verificationObjectId(id)) throw new Error('Invalid ID');
-	let data = await User.findOneAndDelete({_id: id, api_key});
-	if(!data) throw new Error('User not found');
-	return 'User permanently deleted and their records can no longer be accessed!';
+const definitiveDelete = async (id) => {
+  let data = await User.findByIdAndDelete(id);
+  if (!data) throw new Error('User not found');
+  return 'User permanently deleted and their records can no longer be accessed!';
 };
 
 module.exports = {
-	logicalDelete,
-	definitiveDelete
+  logicalDelete,
+  definitiveDelete,
 };
--- routes/recovery.js
+++ routes/recovery.js
@@ -1,21 +1,18 @@
 'use strict';
 const { Router } = require('express');
 const router = Router();
-const { verificationApiKey } = require('../controllers/api_key/verificationApiKey.js');
 const { recoveryUser } = require('../controllers/users/recovery.js');
 
 router.put('/user', async (req, res) => {
-	const { api_key, id } = req.query;
-	try {
-		if(await verificationApiKey(api_key)) {
-			let data = await recoveryUser(api_key, id);
-			res.json({
-				data
-			});
-		};
-	} catch (error) {
-		res.status(400).json({error: error.message});
-	};
+  const { id } = req.query;
+  try {
+    let data = await recoveryUser(id);
+    res.json({
+      data,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
