59f335487615c2efef5203c7f496265062a1e666
GusEngers <gustavoengers6441@gmail.com>
2023-04-11 17:45:08 -0300

    login

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -6,42 +6,45 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verify = Joi.object({
-	email: Joi.string().required().email(),
-	password: Joi.string().min(8).required()
+  email: Joi.string().required().email(),
+  password: Joi.string().min(8).required(),
 });
 
-const validationLog = async (api_key, email, password) => {
-	// -- data syntax --
-	const { error } = verify.validate({ email, password });
-	if(error) throw new Error(error.details[0].message);
+const validationLog = async (email, password) => {
+  // -- data syntax --
+  const { error } = verify.validate({ email, password });
+  if (error) throw new Error(error.details[0].message);
 
-	// -- email verification --
-	const dataEV = await User.findOne({email, api_key});
-	if(!dataEV) throw new Error('The email does not exist!');
+  // -- email verification --
+  const dataEV = await User.findOne({ email });
+  if (!dataEV) throw new Error('The email does not exist!');
 
-	// -- password verification --
-	const dataPV = await bcrypt.compare(password, dataEV.password);
-	if(!dataPV) throw new Error('Invalid password!');
+  // -- password verification --
+  const dataPV = await bcrypt.compare(password, dataEV.password);
+  if (!dataPV) throw new Error('Invalid password!');
 
-	return {
-		data: dataEV,
-		status: true
-	};
+  return {
+    data: dataEV,
+    status: true,
+  };
 };
 
-const login = async (api_key, email, password) => {
-	const verification = await validationLog(api_key, email, password);
+const login = async (email, password) => {
+  const verification = await validationLog(api_key, email, password);
 
-	if(verification.status){
-		const token = jwt.sign({
-			name: verification.data.name,
-			id: verification.data._id
-		}, process.env.TOKEN);
+  if (verification.status) {
+    const token = jwt.sign(
+      {
+        name: verification.data.name,
+        id: verification.data._id,
+      },
+      process.env.TOKEN
+    );
 
-		return token;
-	}
+    return token;
+  }
 };
 
 module.exports = {
-	login
-}
+  login,
+};
--- routes/login.js
+++ routes/login.js
@@ -2,19 +2,15 @@
 const { Router } = require('express');
 const router = Router();
 const { login } = require('../controllers/users/login.js');
-const { verificationApiKey } = require('../controllers/api_key/verificationApiKey.js');
 
 router.post('/', async (req, res) => {
-	const { api_key } = req.query;
-	const { email, password } = req.body;
-	try {
-		if(await verificationApiKey(api_key)) {
-			const token = await login(api_key, email, password);
-			res.header('auth-token', token).json({token});
-		}
-	} catch (error) {
-		res.status(400).json({error: error.message});
-	};
+  const { email, password } = req.body;
+  try {
+    const token = await login(email, password);
+    res.header('auth-token', token).json({ token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
