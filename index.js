5bb4d86f719388473584f33ce9c896b4aa0304ae
GusEngers <gustavoengers6441@gmail.com>
2023-04-11 17:30:57 -0300

    cambio función de conexión a mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const express = require('express');
-const mongoose = require('mongoose');
+const { connect } = require('mongoose');
 const morgan = require('morgan');
 const generate = require('./routes/generate.js');
 const register = require('./routes/register.js');
@@ -17,7 +17,7 @@ const app = express();
 
 let corsOptions = {
   origin: '*',
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -25,24 +25,14 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // -- DB Connect --
-mongoose.connect(process.env.URL_DB);
-const database = mongoose.connection;
+connect(process.env.URL_DB)
+  .then(() => {
+    console.info('Database connected!');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
-database.on('error', (error) => {
-	console.log(`Database failed connected: ${error}`);
-});
-database.once('connected', () => {
-	console.log('Database connected!');
-});
-
-// --	Routes --
-// -- default --
-app.get('*', (req, res) => {
-	res.json({
-		info: 'Has ingresado a una ruta inexistente, para más información sobre los endpoints visita el repositorio del proyecto en GitHub',
-		url: 'https://github.com/GusEngers/user-auth-api'
-	});
-});
 // -- enpoints --
 app.use('/generate', generate);
 app.use('/register', register);
@@ -53,6 +43,5 @@ app.use('/delete', deleteUser);
 app.use('/recovery', recovery);
 
 app.listen(process.env.PORT, () => {
-	console.log(`Server start on port: ${process.env.PORT}`);
+  console.log(`Server start on port: ${process.env.PORT}`);
 });
-
