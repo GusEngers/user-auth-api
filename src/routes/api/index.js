8598ad7aeeccb310c328fe2030c152b82dc49819
Gustavo Engers <gustavoengers6441@gmail.com>
2023-09-30 23:45:15 -0300

    Fix: Arreglo de ruta not found en la sección de la API

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,17 +1,8 @@
 const router = require('express').Router();
-const { LINKS_API } = require('../../utils/constants');
 const user = require('./user');
 const auth = require('./auth');
 
 router.use('/user', user);
 router.use('/auth', auth);
-
-router.get('/*', (req, res) => {
-  res.status(404).json({
-    msg: 'La ruta a la que intenta acceder no existe',
-    status: 404,
-    links: LINKS_API,
-  });
-});
 
 module.exports = router;
--- src/utils/handleNotFound.js
+++ src/utils/handleNotFound.js
@@ -1,3 +1,13 @@
 module.exports = (req, res) => {
+  const { LINKS_API } = require('./constants');
+
+  if(req.originalUrl.substring(0, 5) === '/api/') {
+    return res.status(404).json({
+      msg: 'La ruta a la que intenta acceder no existe',
+      status: 404,
+      links: LINKS_API,
+    });
+  }
+
   res.render('not-found');
 };
