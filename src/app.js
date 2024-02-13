c85899953344f5c0ce70d570bc56e604fb4b1104
GusEngers <gustavoengers6441@gmail.com>
2024-02-13 16:52:48 -0300

    Cambio de directorios para archivos estaticos y vistas

--- src/app.js
+++ src/app.js
@@ -6,35 +6,16 @@ const handleNotFound = require('./utils/handleNotFound');
 const handleHeaderApiKey = require('./utils/handleHeaderApiKey');
 const client = require('./routes/client');
 const api = require('./routes/api');
-const ApiKey = require('./models/api-key');
-const User = require('./models/user');
-const { isProduction } = require('./utils/constants');
 
 const app = express();
 
 app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'client', 'public')));
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/client/views');
 app.use(require('morgan')('dev'));
-
-if (isProduction()) {
-  process.stdout.write('[INFO] Dev mode enabled\n');
-
-  app.get('/todos', async (req, res) => {
-    const api_keys = await ApiKey.find({});
-    const users = await User.find({});
-    res.json({ api_keys, users });
-  });
-  
-  app.get('/borrar', async (req, res) => {
-    await ApiKey.deleteMany({});
-    await User.deleteMany({});
-    res.json('Todo eliminado');
-  });
-}
 
 app.use('/', client);
 app.use('/api', handleCors, handleHeaderApiKey, api);
